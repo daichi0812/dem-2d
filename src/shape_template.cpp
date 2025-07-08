27fb9c2ecb7fb35bf0382eea691cb222589e7c98
Glound <glound.app@gmail.com>
2025-07-08 19:25:12 +0900

    first init

---
+++ src/shape_template.cpp
@@ -0,0 +1,20 @@
+//
+//  Created by Yonghao Yue on 2022/04/02.
+//  Copyright © 2022 Yonghao Yue. All rights reserved.
+//
+
+#define EIGEN_DISABLE_UNALIGNED_ARRAY_ASSERT
+#define EIGEN_DONT_VECTORIZE
+
+#include "shape_template.h"
+#include "circle_template.h"
+#include "polygon_template.h"
+
+ShapeTemplate2D* initializeTemplate( const ShapeTemplateData& in_data )
+{
+    if( in_data.vertex_list.cols() < 3 )
+        return new CircleTemplate2D( in_data.name, in_data.density, in_data.size_mean * 0.5, in_data.size_std * 0.5 );
+    else
+        return new PolygonTemplate2D( in_data.name, in_data.density, in_data.vertex_list, in_data.size_std );
+}
+
---
+++ src/xml_parser.h
@@ -0,0 +1,14 @@
+//
+//  Created by Yonghao Yue on 2020/09/04.
+//
+
+#ifndef xmlparser_h
+#define xmlparser_h
+
+#include <Eigen/Core>
+#include <rapidxml.hpp>
+#include "settings.h"
+
+bool openXMLFile( const std::string& filename, SimulatorSettings& setting );
+
+#endif /* xmlparser_h */
